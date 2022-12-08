198ed6df7ce97e70ea4c1623a2b2e9b3a492490f
Nimroda Matsi <mnimroda@gmail.com>
2022-12-08 15:56:14 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,29 +1,18 @@
 #include <stdio.h>
 
-/**
+/*
+ *main -is the entry point for the program.
+ *Return:zero if no error, Non-zero value  if errors.
  *
- *  *main -is the entry point for the program.
- *
- *   *
- *
- *    *Return:zero if no error, Non-zero value  if errors.
- *
- *     */
+ */
 
 int main(void)
 
 {
-
-		printf("Size of a char: %ld byte(s)\n", sizeof(char));
-
-			printf("Size of an int: %ld byte(s)\n", sizeof(int));
-
-				printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-
-					printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-
-						printf("Size of a float: %ld byte(s)\n", sizeof(float));
-
-							return (0);
-
+	printf("char size: %ld byte(s)\n", sizeof(char));
+	printf("int size: %ld byte(s)\n", sizeof(int));
+	printf("long int size: %ld byte(s)\n", sizeof(long int));
+	printf("long long int size: %ld byte(s)\n", sizeof(long long int));
+	printf("float size: %ld byte(s)\n", sizeof(float));
+return (0);
 }
