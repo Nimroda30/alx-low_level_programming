07383083f61f40ddd6a3dfba983a158e651d7ae4
Nimroda Matsi <mnimroda@gmail.com>
2022-12-09 13:43:51 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: alway returns 0
+ */
+
+int main(void)
+
+{
+
+	int a = 0;
+
+
+
+	for (; a < 10 ; a++)
+
+		printf("%d", a);
+
+	putchar('\n');
+
+	return (0);
+
+}
