8519fd0dc3bba5e8301c04b125f34722ad85590f
Nimroda Matsi <mnimroda@gmail.com>
2022-12-09 13:50:03 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return:returns 0
+ */
+
+int main(void)
+
+{
+
+	int a = 0;
+
+
+
+	for (; a < 10 ; a++)
+
+		putchar('0' + a);
+
+	putchar('\n');
+
+	return (0);
+
+}
