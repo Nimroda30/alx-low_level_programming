9876862feb548498ced2bdc76ddc7147f21d9cbf
Nimroda Matsi <mnimroda@gmail.com>
2023-01-10 15:18:05 -0800

    0-create_array.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+
 char *create_array(unsigned int size, char c);
 
 char *_strdup(char *str);
@@ -14,3 +15,5 @@ void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 
 char **strtow(char *str);
+
+#endif /* MAIN_H */
